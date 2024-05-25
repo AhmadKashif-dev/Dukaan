832bcdc15ecde4b308eff3e3e9f7604d1f4fade1
Kashif Ahmad <kashifahmad124590@gmail.com>
2024-05-25 17:13:11 +0500

    Done with the project, Alhamdulillah

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,6 +7,7 @@ import { createProductReview, listProductDetails } from "../actions/productActio
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { PRODUCT_CREATE_REVIEW_RESET } from "../constants/productConstants";
+import Meta from "../components/Meta";
 
 const ProductScreen = ({ match }) => {
   const [qty, setQty] = useState(1);
@@ -65,6 +66,7 @@ const ProductScreen = ({ match }) => {
         <Message variant={"danger"}>{error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid></Image>
