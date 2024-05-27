933fcf4c9d2ff2a2de03eb0558b71757988961b6
Kashif Ahmad <kashifahmad124590@gmail.com>
2024-05-27 11:03:14 +0500

    Final

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ import uploadRoutes from './routes/uploadRoutes.js'
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import cors from 'cors';
 
+
 dotenv.config();
+connectDB()
+
 const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
@@ -19,26 +22,37 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json())
 app.use(cors());
 
-connectDB()
-
-app.get("/", (req, res) => {
-  res.send("API is running.....with");
-});
 
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-const PORT = process.env.PORT || 5000;
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
