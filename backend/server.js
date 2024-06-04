d742c43eb3b5f6d0eb810513ef1a3d025edc677f
Kashif Ahmad <kashifahmad124590@gmail.com>
2024-06-04 14:21:31 +0500

    checking deployment again

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json())
 app.use(cors());
 
-
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
@@ -36,17 +38,17 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   )
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running....')
+//   })
+// }
 
 app.use(notFound)
 app.use(errorHandler)
