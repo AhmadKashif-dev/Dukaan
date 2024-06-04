edc1f405b0c09c074dc5045d694db42642ce183f
Kashif Ahmad <kashifahmad124590@gmail.com>
2024-06-04 14:28:04 +0500

    checking deployment again

--- backend/server.js
+++ backend/server.js
@@ -12,30 +12,30 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import cors from 'cors';
 
 
-dotenv.config();
-connectDB()
+// dotenv.config();
+// connectDB()
 
 const app = express();
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"))
-}
-app.use(express.json())
-app.use(cors());
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"))
+// }
+// app.use(express.json())
+// app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/orders", orderRoutes)
-app.use('/api/upload', uploadRoutes)
+// app.use("/api/products", productRoutes)
+// app.use("/api/users", userRoutes)
+// app.use("/api/orders", orderRoutes)
+// app.use('/api/upload', uploadRoutes)
 
 
-app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-)
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+// app.get('/api/config/paypal', (req, res) =>
+//   res.send(process.env.PAYPAL_CLIENT_ID)
+// )
+// const __dirname = path.resolve()
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 
 // if (process.env.NODE_ENV === 'production') {
@@ -50,12 +50,12 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 //   })
 // }
 
-app.use(notFound)
-app.use(errorHandler)
+// app.use(notFound)
+// app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
-);
+// app.listen(
+//   PORT,
+//   console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+// );
